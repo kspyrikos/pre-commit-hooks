4838562d3870a94821ae65810be1911d50f45393
Konstantinos Spyrikos <kspyrikos@gmaIl.com>
2024-08-07 12:34:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pre-commit-hooks',
-    version='v0.0.4',
+    version='v0.0.5',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
